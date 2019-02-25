8fe3c590983831800f44e94e30531615d779aef4
crrrrrw <120755274@qq.com>
2019-02-25 23:44:19 +0800

    自定义事务处理器

--- src/main/java/com/gemantic/wealth/EventDrivenDemoApplication.java
+++ src/main/java/com/gemantic/wealth/EventDrivenDemoApplication.java
@@ -2,10 +2,13 @@ package com.gemantic.wealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @EnableAsync
 @EnableTransactionManagement
 public class EventDrivenDemoApplication {
--- src/main/java/com/gemantic/wealth/conf/BeanConfig.java
+++ src/main/java/com/gemantic/wealth/conf/BeanConfig.java
@@ -1,14 +1,42 @@
 package com.gemantic.wealth.conf;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionStatus;
 
+@Slf4j
 @Configuration
 public class BeanConfig {
 
-    // @EnableTransactionManagement 已自动配置
-    /*@Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }*/
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new AbstractPlatformTransactionManager() {
+            @Override
+            protected Object doGetTransaction() throws TransactionException {
+                return new Object();
+            }
+
+            @Override
+            protected void doBegin(Object o, TransactionDefinition transactionDefinition) throws TransactionException {
+                log.info("[transaction] begin...");
+            }
+
+            @Override
+            protected void doCommit(DefaultTransactionStatus defaultTransactionStatus) throws TransactionException {
+                log.info("[transaction] commit...");
+            }
+
+            @Override
+            protected void doRollback(DefaultTransactionStatus defaultTransactionStatus) throws TransactionException {
+                log.info("[transaction] rollback...");
+            }
+        };
+    }
 
 }
